dc80785968847ed6e9854f246359aec81adda75c
Almeidaadavid <davidalmeida0147@gmail.com>
2021-05-09 19:22:18 -0300

    feat: Create movement snake, and function startGame

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
-function criarBG() {
+let direction = "right";
+
+function createBG() {
     context.fillStyle = "lightgreen"
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
@@ -18,5 +20,27 @@ function createSnake() {
     }
 }
 
+function startGame(){ 
+createBG()
+createSnake()
 
-criarBG()
+let snakeX = snake[0].x;
+let snakeY = snake[0].y;
+
+if (direction == 'right') snakeX += box;
+if (direction == 'left') snakeX -= box;
+if (direction == 'up') snakeY -= box;
+if (direction == 'down') snakeY += box
+
+snake.pop();
+
+let newHead = {
+    x: snakeX,
+    y: snakeY
+}
+
+snake.unshift(newHead);
+
+}
+
+let game = setInterval(startGame, 100);
